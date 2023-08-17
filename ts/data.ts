0555a794f92fc9213c46ab2f8dd3fcadc6afbc51
BurakVeziran <burak.veziran@gmail.com>
2023-08-17 23:30:54 +0300

    createForm updated

--- ts/data.ts
+++ ts/data.ts
@@ -2,11 +2,11 @@ import { randomValue } from './randomValueGenerator';
 export const data =
     {baseURL: "https://api.jotform.com",
     timeZone: "Europe/Istanbul",
-    APIKey : "8c851ab0b530d53a97210978b104dec9",
-    username : "burakveziran321",
-    email : "burak.veziran@gmail.com",
-    password : "7HyHpebrBrB^!w",
-    name : "Burak Veziran",
+    APIKey : process.env.APIKEY,
+    username : process.env.USERNAME,
+    email : process.env.EMAIL,
+    password : process.env.PASSWORD,
+    name : process.env.NAME,
 
     randomEmail: randomValue(10)+"@gmail.com",
     randomUsername: randomValue(10),
